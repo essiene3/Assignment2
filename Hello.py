d5578168615ee584798e7ec61d2ffa3581a96e1c
Dr. McLean Essiene <63370567+essiene3@users.noreply.github.com>
2023-10-07 20:38:06 +0100

    Add files via upload
    Create a python dictionary storing a list of members in the following teams (Alpha Team, Beta Team, Brainy Team, Harvard Team, Stars Team)

---
+++ Hello.py
@@ -0,0 +1,25 @@
+
+print ("Assignment2")
+
+#Alpha Team, Beta Team, Brainy Team, Harvard Team, Stars Team
+
+
+Teams ={'AlphaTeam' : 'John , Matthew, George, James' , 'BetaTeam':  'Joe, Ben, Eric, Ken', 'BrainyTeam' : 'Simeon, Sam,  Sony, Levi' ,  'HarvardTeam' : 'Leo, Terry, Obed, Zacc' ,  'StarsTeam' :'Fred, Willy, Oneil, Dean'}
+
+print (Teams) 
+print (Teams.keys())  
+print (Teams.values()) 
+
+
+
+# 10 previous Nigerian presidents with their year of attaining power
+
+dictpastpres10 = {'Goodluck Ebele Jonathan' : 2010, 'Umaru-Musa-Yar-Adua':  2007, 'Olusegun Obasanjo':  1999, 'Gen Abdulsalami Abubakar':  1998, 'General Sani Abacha' : 1993, 'Ernest Shonekan':  1993, 'General Ibrahim Babangida':  1985, 'General Muhammadu Buhari':  1983, 'President Shehu Shagari':  1979, 'General Obasanjo Olusegun' : 1976 }
+print(dictpastpres10)
+
+dictpastpres10 = {'Goodluck Ebele Jonathan' : 2010, 'Umaru-Musa-Yar-Adua':  2007, 'Olusegun Obasanjo':  1999, 'Gen Abdulsalami Abubakar':  1998, 'General Sani Abacha' : 1993, 'Ernest Shonekan':  1993, 'General Ibrahim Babangida':  1985, 'General Muhammadu Buhari':  1983, 'President Shehu Shagari':  1979, 'General Obasanjo Olusegun' : 1976 }
+
+
+
+
+
